9934b04eadba4efd31e2c70a09f0bc9bfae43877
Charles Read <charlesread@clayton.edu>
2020-10-16 08:05:54 -0400

    added installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-cd /tmp
-git clone https://github.com/charlesread/gumdrop.git
-cd gumdrop
 useradd gumdrop -s /sbin/nologin -m
 cp config.yaml /home/gumdrop
 chown gumdrop:gumdrop /home/gumdrop/config.yaml # edit appropriately
