17240b32c15662e0e735fb1b537c76043bd5f8ff
Charles Read <charles@charlesread.com>
2020-10-19 17:03:23 -0400

    more integration testing

--- internal/integration_test/integration_test.go
+++ internal/integration_test/integration_test.go
@@ -3,7 +3,11 @@
 package integration_test
 
 import (
+	"bytes"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"testing"
 )
 
@@ -65,11 +69,51 @@ func TestIntegration(t *testing.T) {
 	t.Run("POST / with correct Authorization and correct content type should return 400", func(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodPost, baseUrl, nil)
 		req.Header.Set("Authorization", "bearer someOtherEquallySuperSecretToken")
-		req.Header.Set("Content-Type", "multipart/form/data")
+		req.Header.Set("Content-Type", "multipart/form-data")
 		client := http.Client{}
 		resp, _ := client.Do(req)
 		got := resp.StatusCode
 		want := http.StatusBadRequest
+		assert(t, got, want)
+	})
+
+	t.Run("good posting works", func(t *testing.T) {
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+ 		part, err := writer.CreateFormFile("file", "sampleUpload.csv")
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		fi, err := os.Open("sampleUpload.csv")
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		data, err := ioutil.ReadAll(fi)
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		_, err = part.Write(data)
+		if err != nil {
+			t.Error(err.Error())
+		}
+		err = writer.Close()
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		req, _ := http.NewRequest(http.MethodPost, baseUrl, body)
+		req.Header.Set("Authorization", "bearer someOtherEquallySuperSecretToken")
+		req.Header.Set("x-directory", "someDirectory")
+		req.Header.Add("Content-Type", writer.FormDataContentType())
+		client := http.Client{}
+		resp, _ := client.Do(req)
+		//b, _ := ioutil.ReadAll(resp.Body)
+		//fmt.Printf("%s\n", b)
+		got := resp.StatusCode
+		want := http.StatusCreated
 		assert(t, got, want)
 	})
 
