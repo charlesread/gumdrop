50d388ad813b84e78eea00dbbfdf3e5f1fbf959e
Charles Read <charles@charlesread.com>
2020-11-09 16:23:14 -0500

    started middlewareAuthentication

--- internal/router.go
+++ internal/router.go
@@ -3,17 +3,22 @@ package internal
 import (
 	"github.com/charlesread/gumdrop/internal/routes"
 	"github.com/gorilla/mux"
+	"net/http"
 	"sync"
 )
 
 var r *mux.Router
+var once sync.Once
+
+func init() {
+	once = sync.Once{}
+}
 
 func NewRouter() *mux.Router {
 
-	once := sync.Once{}
-
 	once.Do(func() {
 		r = mux.NewRouter()
+		r.Use(middlewareAuthentication)
 		r.HandleFunc("/", routes.UploadHandler).Methods("POST")
 		r.HandleFunc("/stat", routes.StatHandler).Methods("GET")
 	})
@@ -22,6 +27,25 @@ func NewRouter() *mux.Router {
 
 }
 
-func GetRouter() *mux.Router {
-	return NewRouter()
+func middlewareAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		bearerHeader := r.Header.Get("Authorization")
+		if bearerHeader == "" {
+			http.Error(w, MsgNoAuth, http.StatusUnauthorized)
+			return
+		}
+		if len(bearerHeader) < 8 {
+			http.Error(w, MsgAuthMalformed, http.StatusUnauthorized)
+			return
+		}
+		token := bearerHeader[7:]
+		tokenValid := validateToken(token)
+		if tokenValid != true {
+			http.Error(w, MsgTokenInvalid, http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
